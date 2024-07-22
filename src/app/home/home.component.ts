7fc296c64d1da1e0c0582b8ec80891065eb27eed
Ernstjan Hoek <ernstjanhoek@tutanota.com>
2024-07-22 12:02:10 +0200

    housing location component + interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   template:`
     <section>
       <form>
@@ -11,10 +14,25 @@ import { Component } from '@angular/core';
         <button class="primary" type="button">Search</button>
       </form>
     </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `
   ,
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 
 }
